5dc878d71687915d968596eae858d51c98df88e2
0AMAN-RAJ0 <amanraj060206@gmail.com>
2025-05-20 16:56:30 +0000

    comparisons_1

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 == 1)
+// console.log(2 >= 1)
+// console.log(2 <= 1)
+// console.log(2 > 1)
+// console.log(2 < 1)
+// console.log(2 != 1)
+
+console.log("2" > 1) // converted to number then compared as true
+console.log("02" > 1) // same as above
+
+/*
+console.log(null == 0) // false -> no conversion of null
+console.log(null >= 0) // true -> conversion of null as 0
+console.log(null <= 0) // true -> conversion of null as 0
+console.log(null > 0) // false -> conversion of null as 0
+console.log(null < 0) // false -> conversion of null as 0
+*/
+console.log(undefined == 0) // false 
+console.log(undefined >= 0) // false
+console.log(undefined <= 0) // false
+console.log(undefined > 0) // false
+console.log(undefined < 0) // false
