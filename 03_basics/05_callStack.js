6212c60f1214de3ebf8e9eef4cb8be61824267de
0AMAN-RAJ0 <amanraj060206@gmail.com>
2025-06-01 11:24:59 +0000

    call_stack

---
+++ 03_basics/05_callStack.js
@@ -0,0 +1,17 @@
+/// JAVASCRIPT EXECUTION CONTEXT ///
+/*
+in the main code file it is -> Global EC
+-> Global execution context (this)
+-> Function exection context
+-> Eval execution context
+
+** code execution occurs in two phases 
+-> Memory creation phase -> only allocates all memory
+-> Execution phase -> here all execution work is done (+, - , *, /)
+
+Call Stack -> LIFO (Last in first Out)
+learningJS snippet in browser
+three()
+two()
+one()
+*/
